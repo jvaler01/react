47962d2f2ef33de67ba8dc947040ddee12e0eb8a
jvalero <jorgevm13@gmail.com>
2022-01-01 21:27:27 +0100

    working on 02 react components first steps, and counter app

---
+++ 02/counter-app/src/FirstApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import React, { Fragment } from "react";
+
+// FC
+const FirstApp = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>My first application</p>
+        </>
+    );
+}
+
+export default FirstApp;
