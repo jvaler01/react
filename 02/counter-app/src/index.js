085a85644254610584624e4755ec58d6bfd7e270
jvalero <jorgevm13@gmail.com>
2022-04-16 20:32:40 +0200

    upgrade to react 18

--- 02/counter-app/src/index.js
+++ 02/counter-app/src/index.js
@@ -1,11 +1,21 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import CounterApp from './CounterApp';
+////import FirstApp from './FirstApp';
+
+//import './index.css';
+
+//const divRoot = document.querySelector('#root');
+
+////ReactDOM.render( <FirstApp someprop='testing props' anotherprop='isRequired'/>, divRoot );
+//ReactDOM.render( <CounterApp value={5}/>, divRoot );
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import CounterApp from './CounterApp';
-//import FirstApp from './FirstApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#root');
-
-//ReactDOM.render( <FirstApp someprop='testing props' anotherprop='isRequired'/>, divRoot );
-ReactDOM.render( <CounterApp value={5}/>, divRoot );
+const root = createRoot(divRoot);
+root.render(<CounterApp value={5}/>);
